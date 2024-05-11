30268399220e7c6c68936419813c1e8a1603ca32
gaedique <gaedique4@gmail.com>
2024-05-11 09:21:06 +0200

    Ajout de la notation d'un livre

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -5,16 +5,26 @@ const resizeImage = async (req, res, next) => {
         return next(); // Ensures that code only executes when a file is actually present
     }
     try {
-        const originalBuffer = req.file.buffer; // Get the original image buffer from Multer
+        let originalBuffer = req.file.buffer; // Get the original image buffer from Multer
+
+        if (!originalBuffer || !Buffer.isBuffer(originalBuffer)) {
+            console.log('Original buffer is empty or not a buffer');
+            return next(); // If the buffer is empty or not a buffer, continue to the next middleware
+        }
+        // Log the buffer size to ensure it's not empty
+        console.log('Original buffer size:', originalBuffer.length);
+
         // Resize the image and convert it back to a buffer
         const resizedBuffer = await sharp(originalBuffer)
             .resize({ width: 463, height: 595, fit:'contain' })
             .toFormat('webp', { quality: 70 })
-            // .webp({ quality:80 })
             .toBuffer(); // Produces a new buffer containing the modified image data
     
         // Replace the original buffer with the resized one
         originalBuffer = resizedBuffer;
+
+        //   // Replace the original buffer with the resized one
+        //   req.file.buffer = resizedBuffer;
     
         next();
     } catch (error) {
